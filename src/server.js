4130d47359fadccfe885cd35c2da8e50895d40a0
xiguatailangsimida <xiguatailangsida@gmail.com>
2024-11-14 09:56:27 +0800

    x

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 80;
+const port = 9999;
 
 // Parse JSON bodies
 app.use(express.json());
